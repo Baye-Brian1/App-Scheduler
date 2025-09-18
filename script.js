096db87632d69d8db39d066d2efc015e8fe134e4
brian <bayebrian89@gmail.com>
2025-09-18 13:44:38 +0100

    my updated work

---
+++ script.js
@@ -0,0 +1,12 @@
+const form=document.querySelector('.schedule');
+const taskTitle= document.getElementById('task-title');
+const taskText= document.getElementById('task-text');
+const taskDate= document.getElementById('date');
+const taskPriority= document.getElementById('priority');
+ const addTaskBtn= document.querySelector('.sub');
+ const container= document.querySelector('.tasks');
+
+ addTaskBtn.addEventListener('click', ()=>{
+  console.log('hello');
+  
+ })
